4e6381b7882250987bda3b45be575c0a0fa23839
w.t.f.is_it <illia.holovashenko@nure.ua>
2025-05-25 18:05:48 +0300

    fixed pdf report generation

--- src/main/java/ua/nure/holovashenko/medvisionspring/util/pdf/PdfImageUtil.java
+++ src/main/java/ua/nure/holovashenko/medvisionspring/util/pdf/PdfImageUtil.java
@@ -5,12 +5,26 @@ import com.lowagie.text.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 import java.nio.file.Files;
 
 public class PdfImageUtil {
 
-    public static Image loadImageFromPath(String filePath) throws IOException, BadElementException {
-        byte[] imageBytes = Files.readAllBytes(new File(filePath).toPath());
+    public static Image loadImageFromPath(String path) throws IOException, BadElementException {
+        byte[] imageBytes;
+
+        if (path.startsWith("http://") || path.startsWith("https://")) {
+            // Завантаження з URL через URI
+            try (InputStream in = URI.create(path).toURL().openStream()) {
+                imageBytes = in.readAllBytes();
+            }
+        } else {
+            // Завантаження з локального шляху
+            imageBytes = Files.readAllBytes(new File(path).toPath());
+        }
+
         return Image.getInstance(imageBytes);
     }
+
 }
